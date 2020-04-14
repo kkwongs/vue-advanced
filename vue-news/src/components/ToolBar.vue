47ef72fd7ebd4659aec9a60cdddfb81977b8d146
kkwongs <kihyun5741@gmail.com>
2020-04-14 15:06:18 +0900

    router -> routes, router mode(history: url # remove)

--- vue-news/src/components/ToolBar.vue
+++ vue-news/src/components/ToolBar.vue
@@ -2,7 +2,9 @@
   <div class="header">
     <router-link to="/news">News</router-link> |
     <router-link to="/ask">Ask</router-link> |
-    <router-link to="/jobs">Jobs</router-link>
+    <router-link to="/jobs">Jobs</router-link> |
+    <router-link to="/user">User</router-link> |
+    <router-link to="/item">Item</router-link>
   </div>
 </template>
 
