34ad95e468a7a069cdd243b5829f05db26bb3a93
kkwongs <kihyun5741@gmail.com>
2020-04-15 23:00:43 +0900

    store(actions, mutations, state)

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-for="user in users">{{ user.title }}</div>
+    <div v-for="user in this.$store.state.news">{{ user.title }}</div>
   </div>
 </template>
 
@@ -8,21 +8,12 @@
 import { fetchNewsList } from '../api/index.js'
 
 export default {
-  data() {
-    return {
-      users: []
-    }
-  },
   created() {
-    var vm = this;
-    fetchNewsList()
-      .then(function(response) {
-        console.log(response);
-        vm.users = response.data;
-      })
-      .catch(function(error) {
-        console.log(error);    
-      })
+    this.$store.dispatch('FETCH_NEWS');
+
+    // fetchNewsList()
+    //   .then(response => this.users = response.data)
+    //   .catch(error => console.log(error))
   }
 }
 </script>
