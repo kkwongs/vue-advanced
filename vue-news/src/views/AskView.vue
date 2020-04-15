661c3ef0659a148bfe8408852086902ba317347d
kkwongs <kihyun5741@gmail.com>
2020-04-15 23:23:35 +0900

    (JobsView, AskView) apply store

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,22 +1,13 @@
 <template>
   <div>
-    <div v-for="item in ask">{{ item.title }} </div>
+    <div v-for="item in this.$store.state.ask">{{ item.title }} </div>
   </div>
 </template>
 
 <script>
-import { fetchAskList } from '../api/index.js';
-
 export default {
-  data() {
-    return {
-      ask: []
-    }
-  },
   created() {
-    fetchAskList()
-      .then(response => this.ask = response.data)
-      .catch(error => console.log(error))
+    this.$store.dispatch('FETCH_ASK');
   },
 }
 </script>
