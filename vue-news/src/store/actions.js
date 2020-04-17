78e9d7250b92185c420b6b14d21ae12b3698c5ee
kkwongs <kihyun5741@gmail.com>
2020-04-17 11:58:19 +0900

    Dynamic Route matching(itemView)

--- vue-news/src/store/actions.js
+++ vue-news/src/store/actions.js
@@ -2,7 +2,8 @@ import {
   fetchNewsList, 
   fetchJobsList, 
   fetchAskList,
-  fetchUserInfo } 
+  fetchUserInfo, 
+  fetchCommentItem} 
   from "../api/index.js";
 
 export default {
@@ -36,6 +37,15 @@ export default {
     fetchUserInfo(name)
       .then(({ data }) => {
         commit('SET_USER', data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  },
+  FETCH_ITEM({ commit }, id) {
+    fetchCommentItem(id)
+      .then(({ data }) => {
+        commit('SET_ITEM', data)
       })
       .catch(error => {
         console.log(error);
