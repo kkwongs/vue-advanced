f8ecad3a201b02f2acd58c72e6eb2df123b384b5
kkwongs <kihyun5741@gmail.com>
2020-04-19 14:07:06 +0900

    페이지별 데이터 분기(computed)

--- vue-news/src/components/ListItem.vue
+++ vue-news/src/components/ListItem.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <ul class="news-list">
-      <li v-for="item in this.$store.state.news" class="post">
+      <li v-for="item in ListItems" class="post">
           <!-- 포인트 영역 -->
         <div class="points">
-          {{ item. points }}
+          {{ item. points || 0 }}
         </div>
         <!-- 기타 정보 영역 -->
         <div>
@@ -28,8 +28,6 @@
 <script>
 export default {
   created() {
-    // this.$store.dispatch('FETCH_NEWS');
-    // console.log(this.$route.path === '/news');
     const name = this.$route.name;
     if(name === 'news') {
       this.$store.dispatch('FETCH_NEWS');
@@ -39,6 +37,18 @@ export default {
       this.$store.dispatch('FETCH_JOBS');
     }
   },
+  computed: {
+    ListItems() {
+      const name = this.$route.name;
+      if(name === 'news') {
+        return this.$store.state.news;
+      } else if(name === 'ask') {
+        return this.$store.state.ask;
+      } else if(name === 'jobs') {
+        return this.$store.state.jobs;
+      }
+    }
+  }
 }
 </script>
 
--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -26,19 +26,10 @@
 
 <script>
 import ListItem from '../components/ListItem.vue';
-// import { mapState, mapGetters } from 'vuex';
 
 export default {
   components: {
     ListItem,
   },
-  // computed: {
-  //   ...mapGetters([
-  //     'fetchedAsk'
-  //   ]),
-  // },
-  // created() {
-  //   this.$store.dispatch('FETCH_ASK');
-  // },
 }
 </script>
