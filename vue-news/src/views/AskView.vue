7fc5d7daf5a143c678f00181e717bdfafb39ab52
kkwongs <kihyun5741@gmail.com>
2020-04-19 14:26:15 +0900

    template 분기

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,26 +1,6 @@
 <template>
   <div>
     <list-item></list-item>
-    <!-- <ul class="news-list">
-      <li v-for="item in fetchedAsk" class="post">
-        <div class="points">
-          {{ item.points }}
-        </div>
-        <div>
-          <p class="news-title">
-            <router-link v-bind:to="`item/${item.id}`">
-              {{ item.title }}
-            </router-link>
-          </p>
-          <small class="link-text">
-            {{ item.time_ago }} by
-            <router-link v-bind:to="`/user/${item.user}`" class="link-text">
-            {{ item. user }}
-            </router-link>
-          </small>
-        </div>
-      </li>
-    </ul> -->
   </div>
 </template>
 
--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,26 +1,6 @@
 <template>
   <div>
     <list-item></list-item>
-    <!-- <ul class="news-list">
-      <li v-for="job in this.$store.state.jobs" class="post">
-        <div class="points">
-          {{ job.points || 0}}
-        </div>
-        <div>
-          <p class="news-title">
-            <a :href="job.url">
-            {{ job.title }}
-            </a>
-          </p>
-          <small class="link-text">
-            {{ job.time_ago }} by
-            <a :href="job.url">
-              {{ job. domain }}
-            </a>
-          </small>
-        </div>
-      </li>
-    </ul> -->
   </div>
 </template>
 
