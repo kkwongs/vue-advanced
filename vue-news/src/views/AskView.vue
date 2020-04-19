014d1fc8fb74eee87ab888e73e6cb7cd9266f451
kkwongs <kihyun5741@gmail.com>
2020-04-19 13:58:56 +0900

    페이지별 데이터 분기

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <ul class="news-list">
+    <list-item></list-item>
+    <!-- <ul class="news-list">
       <li v-for="item in fetchedAsk" class="post">
         <div class="points">
           {{ item.points }}
@@ -19,48 +20,25 @@
           </small>
         </div>
       </li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex';
+import ListItem from '../components/ListItem.vue';
+// import { mapState, mapGetters } from 'vuex';
 
 export default {
-  computed: {
-    ...mapGetters([
-      'fetchedAsk'
-    ]),
+  components: {
+    ListItem,
   },
-  created() {
-    this.$store.dispatch('FETCH_ASK');
-  },
+  // computed: {
+  //   ...mapGetters([
+  //     'fetchedAsk'
+  //   ]),
+  // },
+  // created() {
+  //   this.$store.dispatch('FETCH_ASK');
+  // },
 }
 </script>
-
-<style scoped>
-.news-list {
-  margin: 0;
-  padding: 0;
-}
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-.points {
-  width: 80px;
-    height: 60px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: #42b883;
-}
-.news-title {
-  margin: 0;
-}
-.link-text {
-  color: #828282;
-}
-</style>
