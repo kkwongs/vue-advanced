76862964ec102552aa4b31cf1c4d50a6179a57c3
kkwongs <kihyun5741@gmail.com>
2020-04-19 16:38:06 +0900

    UserProfile data flow processing

--- vue-news/src/components/UserProfile.vue
+++ vue-news/src/components/UserProfile.vue
@@ -5,10 +5,11 @@
           <i class="fas fa-user"></i>
         </div>
         <div class="user-description">
-          <!-- <router-link :to="`/user/${fetchedItem.user}`">{{ fetchedItem.user }}</router-link>
+          <div>{{ userInfo.id }}</div>
+          <!-- <router-link :to="`/user/${userInfo.user}`">{{ userInfo.id }}</router-link> -->
           <div class="time">
-            {{ fetchedItem.time_ago }}
-          </div> -->
+            {{ userInfo.created }}
+          </div>
         </div>
       </div>
   </div>
@@ -16,7 +17,11 @@
 
 <script>
 export default {
-
+  computed: {
+    userInfo() {
+      return this.$store.state.user;
+    }
+  },
 }
 </script>
 
--- vue-news/src/views/UserView.vue
+++ vue-news/src/views/UserView.vue
@@ -14,16 +14,15 @@ export default {
   components: {
     UserProfile,
   },
-  computed: {
-    userInfo() {
-      return this.$store.state.user;
-    }
-  },
+  // computed: {
+  //   userInfo() {
+  //     return this.$store.state.user;
+  //   }
+  // },
   created() {
     const userName = this.$route.params.id;
-    // axios.get(`https://api.hnpwa.com/v0/user/${userName}.json`);
     this.$store.dispatch('FETCH_USER', userName);
-  }
+  },
 }
 </script>
 
