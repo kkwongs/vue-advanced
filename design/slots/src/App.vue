0ab0ce2bd8e3eec51038cc96bd4c0c3b6b738d1a
kkwongs <kihyun5741@gmail.com>
2020-04-23 17:26:49 +0900

    Slot vs Props

---
+++ design/slots/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul>
+      <!-- <item>아이템 1</item>
+      <item>아이템 2</item>
+      <item>아이템 3</item>
+      <item>아이템 4</item>
+      <item>아이템 5</item> -->
+      <item v-bind:key="item" v-for="item in items" :propsdata="item"></item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from './Item.vue';
+
+export default {
+  components: {
+    Item,
+  },
+  data() {
+    return {
+      items: ['아이템1', '아이템2', '아이템3', '아이템4', '아이템5']
+    }
+  }
+}
+</script>
